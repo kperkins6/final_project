206429b509214042687ab72894514d69b62fd173
kperkins6 <kperkins6@mail.csuchico.edu>
2015-12-11 22:37:23 -0800

    Created A Show Page for Each Message Board

--- skylonda/app/models/post.rb
+++ skylonda/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :board
+  has_many :comments, :dependent => destroy
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200"}
 
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/gif']
