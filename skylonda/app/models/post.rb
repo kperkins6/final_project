05c02f42f124da4de296aca487acdd8100849ed9
kperkins6 <kperkins6@mail.csuchico.edu>
2015-12-11 22:19:08 -0800

    Added code for paperclip attachment

--- skylonda/app/models/post.rb
+++ skylonda/app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :board
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200"}
+
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+  validates_attachment_content_type :document, :content_type =>['application/pdf']
 end
